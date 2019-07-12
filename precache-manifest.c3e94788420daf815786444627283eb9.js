dfcc496cf117253ec1768cf4e4e5b4a9b9e20dad
Patrycja Paczkowska <umbra@spoko.pl>
2019-07-12 14:36:28 +0200

    Updates

---
+++ precache-manifest.c3e94788420daf815786444627283eb9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ac398c2d9ef878c1de34618a2a2e84",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "108e0657b2c8bf0f1283",
+    "url": "/Portfolio/static/css/main.0f665052.chunk.css"
+  },
+  {
+    "revision": "399dda526841ec035bd6",
+    "url": "/Portfolio/static/js/2.12101c11.chunk.js"
+  },
+  {
+    "revision": "108e0657b2c8bf0f1283",
+    "url": "/Portfolio/static/js/main.dc7dfd69.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "6095b258d9bbdc1ec750b523687a7bec",
+    "url": "/Portfolio/static/media/calc.6095b258.jpg"
+  },
+  {
+    "revision": "0a5c0dc550311afb043aded993229f6c",
+    "url": "/Portfolio/static/media/noimages.0a5c0dc5.png"
+  },
+  {
+    "revision": "40e778819be3314d8da740c3b85041db",
+    "url": "/Portfolio/static/media/portfolio.40e77881.jpg"
+  },
+  {
+    "revision": "a39276a4bb233bbbfad9e4c2b2580e24",
+    "url": "/Portfolio/static/media/salonpiekna.a39276a4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.ba285a83204747992abe75705c8a8fe2.js"
+  "/Portfolio/precache-manifest.c3e94788420daf815786444627283eb9.js"
 );
 
 self.addEventListener('message', (event) => {
