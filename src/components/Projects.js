3edd9281acb25fcdba4ec9b0108ea21a802ba952
Patrycja Paczkowska <umbra@spoko.pl>
2019-08-08 21:12:06 +0200

    add links to new projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,9 @@ import salon from './img/salonpiekna.jpg';
 import calc from './img/calc.jpg';
 import port from './img/portfolio.jpg';
 import noimages from './img/noimages.png';
+import save from './img/savings.jpg';
+import todo from './img/todo.jpg';
+import simplePage from './img/simplePage.jpg';
 
 class Projects extends React.Component {
   componentDidMount = () => {
@@ -36,10 +39,10 @@ class Projects extends React.Component {
         <article className="project" ref="right">
         <img src={salon} alt="Beauty Project" />
 
-<div className="description">
-<a href="https://umbbra.github.io/SalonPieknosci/"><h3>Salon Piękności</h3></a>
-  Strona poświęcona fikcyjnej firmie. W trakcie tworzenia używałam podstawowych języków (HTML, CSS, JS), aby doszliwować umiejętności i przypomnieć niektóre podstawy.
-</div>
+        <div className="description">
+        <a href="https://umbbra.github.io/SalonPieknosci/"><h3>Salon Piękności</h3></a>
+          Strona poświęcona fikcyjnej firmie. W trakcie tworzenia używałam podstawowych języków (HTML, CSS, JS), aby doszliwować umiejętności i przypomnieć niektóre podstawy.
+        </div>
         </article>
         <article className="project" ref="left">
 
@@ -55,28 +58,50 @@ class Projects extends React.Component {
 
         <div className="description">
           <a href="https://umbbra.github.io/Kalkulatory/"><h3>Kalkulator </h3></a>
-          Na stronie znajduje się kalkulator zwykły i walutowy. Stylowanie w SCSS. Pobieranie aktualnych danych za pomocą Fetch Api ze strony http://api.nbp.pl/.
+          Na stronie znajduje się kalkulator zwykły i walutowy. Stylowanie w SCSS. 
         </div>
         </article>
-        <article className="project"  ref="left">
+        <article className="project" ref="left">
           
-          <img src={noimages} alt="Menu Project" />
+          <img src={save} alt="Savings Project" />
 
           <div className="description">
-            <a href="https://umbbra.github.io/Portfolio/"><h3>Jadłospis</h3></a>
-            Do aplikacji można wpisać dania wraz ze składnikami. Następnie
-            wybieramy jadłospis na kolejny dzień i generujemy listę zakupów.
+            <a href="https://umbbra.github.io/savings/"><h3>Oszczędności</h3></a>
+            Ta aplikacja pomoże nam zachować systematyczność i motywację do oszczędzania. Po wpisaniu celu naszego oszczędzania i kolejnych wpłat aplikacja oblicza ile jeszcze pozostało do spełnienia marzenia.
           </div>
         </article>
         <article className="project" ref="right">
           
-          <img src={noimages} alt="Savings Project" />
+          <img src={todo} alt="Savings Project" />
 
           <div className="description">
-            <a href="https://umbbra.github.io/Portfolio/"><h3>Oszczędności</h3></a>
-            Ta aplikacja pomoże nam zachować systematyczność i motywację do oszczędzania. Po wpisaniu celu naszego oszczędzania i kolejnych wpłat aplikacja oblicza ile jeszcze pozostało do spełnienia marzenia.
+            <a href="https://umbbra.github.io/todoapp/"><h3>Zwykła aplikacja To-Do</h3></a>
+            W tym miejscu możemy zaplanować nasz dzień. Strona zrobiona wraz z kursem na stronie Udemy. Jednak, aby mogła działać na moich zasadach kod został gruntownie zmieniony.
           </div>
         </article>
+
+        <article className="project" ref="left">
+          
+          <img src={simplePage} alt="Savings Project" />
+
+          <div className="description">
+            <a href="https://umbbra.github.io/Flexbox-simplyPage/"><h3>Prosty design</h3></a>
+            Podstawą działania struktury strony jest Flexbox. Nic specjalnego.
+          </div>
+        </article>
+
+        <article className="project"  ref="right">
+          
+          <img src={noimages} alt="Menu Project" />
+
+          <div className="description">
+            <a href="/"><h3>Jadłospis</h3></a>
+            Uwaga! Aplikacja w budowie!
+            Do aplikacji będzie można wpisać dania wraz ze składnikami. Następnie
+            wybrać jadłospis na kolejny dzień i wygenerować listę zakupów.
+          </div>
+        </article>
+        
       </section>
     );
   }
